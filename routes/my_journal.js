9f90e17abdf407f4907b366a741d20bff98bd317
muybienw <mubin.w@gmail.com>
2016-02-26 04:51:26 +0000

    tweaked signin page

--- routes/my_journal.js
+++ routes/my_journal.js
@@ -7,75 +7,53 @@ var models = require('../models');
 
 exports.view = function(req, res){
 
-    if(!req.session.user) {
-        res.redirect('/');
-    }
-    else {
-        console.log(req.session.user);
 
-        var searchOption = {"name": req.session.user.name}
-        console.log(searchOption);
+    console.log(req.session.user);
 
-        models.User.findOne(searchOption).populate('journals').sort({createdAt: -1}).exec(function (err, result) {
-            if (err) return console.error(err);
+    var searchOption = {"name": req.session.user.name}
+    console.log(searchOption);
 
-            var list = result['journals'];
-            //console.log(list);
+    models.User.findOne(searchOption).populate('journals').sort({createdAt: -1}).exec(function (err, result) {
+        if (err) return console.error(err);
 
-            function compare(a, b) {
-                //console.log("compare:" )
-                //console.log(a.createdAt);
-                //console.log(b.createdAt);
-                //console.log(b.createdAt - a.createdAt);
-                //console.log();
+        var list = result['journals'];
+        //console.log(list);
 
-                return b.createdAt - a.createdAt;
-            }
-
-            //console.log();
-            //console.log();
+        function compare(a, b) {
+            //console.log("compare:" )
+            //console.log(a.createdAt);
+            //console.log(b.createdAt);
+            //console.log(b.createdAt - a.createdAt);
             //console.log();
 
-            list.sort(compare);
-            //console.log(list);
+            return b.createdAt - a.createdAt;
+        }
 
-            res.render('my_journal', {
-                'journals': result['journals'], 'user': result,
-                helpers: {
-                    formatDate: function (datetime) {
-                        var date = new Date(datetime);
-                        var options = {
-                            weekday: "long",
-                            year: "numeric",
-                            month: "short",
-                            day: "numeric",
-                            //hour: "2-digit",
-                            //minute: "2-digit"
-                        }
+        //console.log();
+        //console.log();
+        //console.log();
 
-                        return date.toLocaleDateString("en-US", options);
-                    },
+        list.sort(compare);
+        //console.log(list);
 
-                    countJournals: function (journals) {
-                        console.log(journals);
-                        console.log(typeof journals);
-                        return journals.length;
-                    },
-                    countCollaborators: function (collaborators) {
-                        console.log(collaborators);
-                        return collaborators.length;
+        res.render('my_journal', {
+            'journals': result['journals'], 'user': result,
+            helpers: {
+                formatDate: function (datetime) {
+                    var date = new Date(datetime);
+                    var options = {
+                        weekday: "long",
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                        //hour: "2-digit",
+                        //minute: "2-digit"
                     }
-                }
-            });
+
+                    return date.toLocaleDateString("en-US", options);
+                },
+
+            }
         });
-    }
-
-
-   //res.render('my_journal', data);;
-    //function tmp(err, res){
-    //
-    //    console.log(res);
-    //    console.log(res[0]["journals"]);
-    //    console.log(res[0]["name"]);
-    //}
+    });
 };
